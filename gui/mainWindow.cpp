8f28b0047e97699b7377b05ace4216795f4b7e51
Vahan Aghajanyan <vahancho@gmail.com>
2011-10-25 22:58:21 +0200

    Added first gui related code - the main window implementation stubs.
    This class' code reused from my another incomplete project "Test results
    dashbard" and contain basic gui such as MDI windows support, basic
    menus and actions. Functionality will be added later.

---
+++ gui/mainWindow.cpp
@@ -0,0 +1,449 @@
+/**************************************************************************
+*   Copyright (C) 2011 by Vahan Aghajanyan                                *
+*   vahancho@gmail.com                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include <QtGui>
+#include "mainWindow.h"
+#include "../strings/strings.h"
+#include "../strings/guiStrings.h"
+
+namespace gui
+{
+
+// Default window size and position
+const int defSizeX  = 800;
+const int defSizeY  = 700;
+const int defPosX   = 200;
+const int defPosY   = 200;
+
+// Tool bars icon size
+const int iconSizeX = 16;
+const int iconSizeY = 16;
+
+// Constructor
+MainWindow::MainWindow(QWidget *parent, Qt::WFlags flags)
+    :
+        QMainWindow(parent, flags),
+        m_trayIcon(0)
+{
+    createMenus();
+
+    m_mdiArea.setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    m_mdiArea.setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    // Set MDI area as a central widget
+    setCentralWidget(&m_mdiArea);
+
+    // Handle MDI sub windows activation.
+    connect(&m_mdiArea, SIGNAL(subWindowActivated(QMdiSubWindow *)),
+            this, SLOT(onSubWindowActivated(QMdiSubWindow *)));
+
+    m_windowMapper = new QSignalMapper(this);
+
+    // Set MDI sub windows mapping.
+    connect(m_windowMapper, SIGNAL(mapped(QWidget *)),
+            this, SLOT(setActiveSubWindow(QWidget *)));
+
+    // Set window title
+    setWindowTitle(str::sAppName);
+
+    // Register Main Window defaults
+    addDefaults();
+
+    if(QSystemTrayIcon::isSystemTrayAvailable())
+        createTrayIcon();
+}
+
+// Destructor
+MainWindow::~MainWindow()
+{}
+
+void MainWindow::createTrayIcon()
+{
+    QMenu *trayIconMenu = new QMenu(this);
+
+    QAction *action = new QAction(str::ActionMinimize, this);
+    connect(action, SIGNAL(triggered()), this, SLOT(hide()));
+    trayIconMenu->addAction(action);
+
+    action = new QAction(str::ActionMaximize, this);
+    connect(action, SIGNAL(triggered()), this, SLOT(showMaximized()));
+    trayIconMenu->addAction(action);
+
+    action = new QAction(str::ActionRestore, this);
+    connect(action, SIGNAL(triggered()), this, SLOT(showNormal()));
+    trayIconMenu->addAction(action);
+
+    trayIconMenu->addSeparator();
+
+    action = new QAction(str::ActionQuit, this);
+    action->setIcon(QIcon(":icons/exit"));
+    connect(action, SIGNAL(triggered()), qApp, SLOT(quit()));
+    trayIconMenu->addAction(action);
+
+    m_trayIcon = new QSystemTrayIcon(QIcon(":/icons/app"), this);
+
+    connect(m_trayIcon, SIGNAL(messageClicked()),
+            this, SLOT(trayIconMessageClicked()));
+
+    connect(m_trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(trayIconActivated(QSystemTrayIcon::ActivationReason)));
+
+    m_trayIcon->setContextMenu(trayIconMenu);
+    m_trayIcon->setToolTip(str::sAppName);
+    m_trayIcon->setVisible(true);
+}
+
+void MainWindow::trayIconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (reason) {
+    case QSystemTrayIcon::Trigger:
+    case QSystemTrayIcon::DoubleClick:
+
+    if(isMinimized() || !isVisible() || !isActiveWindow()) {
+        showNormal();
+        activateWindow();
+        raise();
+    }
+
+    break;
+
+    case QSystemTrayIcon::MiddleClick:
+
+        break;
+    default:
+        ;
+    }
+}
+
+void MainWindow::trayIconMessageClicked()
+{}
+
+void MainWindow::onAbout()
+{
+    QMessageBox::about(this,
+                       tr("About Oragir"),
+                       "Oragir v0.1"
+                       "<BR>"
+                       "Copyright © 2011 <a href=\"mailto:vahancho@gmail.com\">Vahan Aghajanyan</a>."
+                       "<BR>"
+                       "All Rights Reserved."
+                       "<BR><BR>"
+                       "Oragir is made possible by the <a href=\"http://sourceforge.net/projects/oragir\">Oragir</a> open source project.");
+}
+
+void MainWindow::createDockWindows()
+{}
+
+void MainWindow::createMenus()
+{
+    //////////////////////////////////////////////////////////////////////////
+    // File menu
+    //
+    QMenu *fileMenu = new QMenu(str::MenuFile, this);
+    QToolBar *fileToolBar = new QToolBar(str::MenuFile, this);
+    fileToolBar->setObjectName(str::MenuFile);
+    fileToolBar->setIconSize(QSize(iconSizeX, iconSizeY));
+
+    QMenu *newMenu = fileMenu->addMenu(str::MenuNew);
+
+    fileMenu->addSeparator();
+    QAction *quitAction = fileMenu->addAction(str::ActionExit);
+    quitAction->setShortcut(QKeySequence(tr("Ctrl+Q")));
+    quitAction->setIcon(QIcon(":icons/exit"));
+    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+    //////////////////////////////////////////////////////////////////////////
+    // View menu
+    //
+    QMenu *viewMenu = new QMenu(str::MenuView, this);
+
+    //QToolBar *viewToolBar = new QToolBar(str::MenuView, this);
+    //viewToolBar->setObjectName(str::MenuView);
+    //viewToolBar->setIconSize(QSize(iconSizeX, iconSizeY));
+
+    // Add 'Views' sub menu
+    m_viewsMenu = viewMenu->addMenu(str::MenuViews);
+    connect(m_viewsMenu, SIGNAL(aboutToShow()), this, SLOT(updateViewsMenu()));
+
+    // Add 'Toolbars' sub menu
+    m_toolBarsMenu = viewMenu->addMenu(str::MenuToolBars);
+    connect(m_toolBarsMenu, SIGNAL(aboutToShow()), this, SLOT(updateToolBarsMenu()));
+
+    // Add StatusBar action
+    m_statusBarAction = viewMenu->addAction(str::ActionStatusBar);
+    m_statusBarAction->setCheckable(true);
+
+    // Set connection
+    connect(m_statusBarAction, SIGNAL(triggered()), this, SLOT(onStatusBarShowHide()));
+
+    //////////////////////////////////////////////////////////////////////////
+    // Window menu
+    m_windowMenu = new QMenu(str::MenuWindow, this);
+    connect(m_windowMenu, SIGNAL(aboutToShow()), this, SLOT(updateWindowMenu()));
+
+    m_closeAct = new QAction(str::ActionClose, this);
+    m_closeAct->setShortcut(tr("Ctrl+F4"));
+    //m_closeAct->setStatusTip(str::MsgCloseActiveWnd);
+    connect(m_closeAct, SIGNAL(triggered()), &m_mdiArea, SLOT(closeActiveSubWindow()));
+
+    m_closeAllAct = new QAction(str::ActionCloseAll, this);
+    //m_closeAllAct->setStatusTip(str::MsgCloseAllWnd);
+    connect(m_closeAllAct, SIGNAL(triggered()),	&m_mdiArea, SLOT(closeAllSubWindows()));
+
+    m_tileAct = new QAction(str::ActionTile, this);
+    //m_tileAct->setStatusTip(str::MsgTile);
+    m_tileAct->setIcon(QIcon(":icons/tile"));
+    connect(m_tileAct, SIGNAL(triggered()), &m_mdiArea, SLOT(tileSubWindows()));
+
+    m_cascadeAct = new QAction(str::ActionCascade, this);
+    //m_cascadeAct->setStatusTip(str::MsgCascade);
+    m_cascadeAct->setIcon(QIcon(":icons/cascade"));
+    connect(m_cascadeAct, SIGNAL(triggered()), &m_mdiArea, SLOT(cascadeSubWindows()));
+
+    m_nextAct = new QAction(str::ActionNext, this);
+    //m_nextAct->setStatusTip(str::MsgNext);
+    connect(m_nextAct, SIGNAL(triggered()), &m_mdiArea, SLOT(activateNextSubWindow()));
+
+    m_previousAct = new QAction(str::ActionPrevious, this);
+    //m_previousAct->setStatusTip(str::MsgPrevious);
+    connect(m_previousAct, SIGNAL(triggered()),	&m_mdiArea, SLOT(activatePreviousSubWindow()));
+
+    m_separatorAct = new QAction(this);
+    m_separatorAct->setSeparator(true);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Help menu
+    QMenu *helpMenu = new QMenu(str::MenuHelp, this);
+    QAction *aboutAction = helpMenu->addAction(str::ActionAbout);
+    connect(aboutAction, SIGNAL(triggered()), this, SLOT(onAbout()));
+
+    //////////////////////////////////////////////////////////////////////////
+
+    // Add menus to menu bar
+    menuBar()->addMenu(fileMenu);
+    menuBar()->addMenu(viewMenu);
+    //menuBar()->addMenu(toolsMenu);
+    menuBar()->addMenu(m_windowMenu);
+    menuBar()->addSeparator();
+    menuBar()->addMenu(helpMenu);
+
+    // Add tool bars
+    addToolBar(fileToolBar);
+    //addToolBar(viewToolBar);
+    //addToolBar(toolsToolBar);
+}
+
+void MainWindow::updateToolBarsMenu()
+{
+    Q_ASSERT(m_toolBarsMenu);
+
+    // Clear menu
+    m_toolBarsMenu->clear();
+
+    // Get the list of all tool bars
+    QList<QToolBar *> toolbars = qFindChildren<QToolBar *>(this);
+
+    if (toolbars.size() != 0) {
+        // If the list is not empty
+
+        // Iterate through all tool bars
+        for (int i = 0; i < toolbars.size(); ++i) {
+            if (toolbars.at(i)->parentWidget() == this)
+                // If the parent of tool bar is main window add toggled action to menu
+                m_toolBarsMenu->addAction(toolbars.at(i)->toggleViewAction());
+        }
+    }
+}
+
+void MainWindow::onStatusBarShowHide()
+{
+    if (statusBar()->isVisible())
+        // If status bar visible hide status bar
+        statusBar()->setVisible(false);
+    else
+        statusBar()->setVisible(true);
+}
+
+void MainWindow::onToolsOptions()
+{}
+
+void MainWindow::saveWindow() const
+{
+    /*
+    core::DefaultManager *defaultMngr = core::Application::theApp()->defaultManager();
+
+    // Save Main Window configuration to defaults
+    defaultMngr->setValue(str::MainWindowSize, size());
+    defaultMngr->setValue(str::MainWindowPos, pos());
+    defaultMngr->setValue(str::MainWindowMax, isMaximized());
+    defaultMngr->setValue(str::MainWindowState, saveState());
+    defaultMngr->setValue(str::ShowStatusBar, m_statusBarAction->isChecked());
+    */
+}
+
+void MainWindow::restoreWindow()
+{
+    /*
+    // Read Main Window configuration from defaults and restore the state
+    core::DefaultManager *defaultMngr = core::Application::theApp()->defaultManager();
+
+    // Set maximized state
+    bool max = defaultMngr->value(str::MainWindowMax).toBool();
+    if(max)
+        showMaximized();
+
+    // Set window size
+    QSize size = defaultMngr->value(str::MainWindowSize).toSize();
+    resize(size);
+
+    // Set window position
+    QPoint pos = defaultMngr->value(str::MainWindowPos).toPoint();
+    move(pos);
+
+    // Restore tool bars state
+    QByteArray state = defaultMngr->value(str::MainWindowState).toByteArray();
+    restoreState(state);
+
+    // Restore status bar state
+    bool showStatus = defaultMngr->value(str::ShowStatusBar).toBool();
+    statusBar()->setVisible(showStatus);
+    m_statusBarAction->setChecked(showStatus);
+    */
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    /*
+    core::DefaultManager *defaultMngr = 
+                            core::Application::theApp()->defaultManager();
+
+    bool quit = defaultMngr->value(str::QuitOnClose).toBool();
+
+    if(!quit) {
+        hide();
+        event->ignore();
+    } else
+        event->accept();
+
+    saveWindow();
+    */
+}
+
+void MainWindow::addDefaults() const
+{
+    /*
+    core::DefaultManager *defaultMngr = core::Application::theApp()->defaultManager();
+
+    // Add Main Window related defaults with initial and factory values.
+    defaultMngr->addProperty(str::MainWindowSize, QSize(defSizeX, defSizeY), QSize(defSizeX, defSizeY));
+    defaultMngr->addProperty(str::MainWindowPos, QPoint(defPosX, defPosY), QPoint(defPosX, defPosY));
+    defaultMngr->addProperty(str::MainWindowMax, bool(false), bool(false));
+    defaultMngr->addProperty(str::MainWindowState, QByteArray(), QByteArray());
+    defaultMngr->addProperty(str::ShowStatusBar, bool(true), bool(true));
+    defaultMngr->addProperty(str::RecentDialogState, QByteArray(), QByteArray());
+    defaultMngr->addProperty(str::QuitOnClose, bool(false), bool(false));
+    */
+}
+
+void MainWindow::updateWindowMenu()
+{
+    // Clear menu actions to add new ones
+    m_windowMenu->clear();
+
+    QList<QMdiSubWindow *> windows = m_mdiArea.subWindowList();
+
+    m_windowMenu->addAction(m_closeAct);
+    m_closeAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addAction(m_closeAllAct);
+    m_closeAllAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addSeparator();
+
+    m_windowMenu->addAction(m_tileAct);
+    m_tileAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addAction(m_cascadeAct);
+    m_cascadeAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addSeparator();
+
+    m_windowMenu->addAction(m_nextAct);
+    m_nextAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addAction(m_previousAct);
+    m_previousAct->setEnabled(!windows.isEmpty());
+    m_windowMenu->addAction(m_separatorAct);
+
+    m_separatorAct->setVisible(!windows.isEmpty());
+
+    // Add actions corresponding to MDI sub windows
+    for (int i = 0; i < windows.size(); ++i) {
+        QMdiSubWindow *child = windows.at(i);
+
+        QString text;
+
+        if (i < 9)
+            text = tr("&%1 %2").arg(i + 1).arg(child->windowTitle());
+        else
+            text = tr("%1 %2").arg(i + 1).arg(child->windowTitle());
+
+        QAction *action  = m_windowMenu->addAction(text);
+        action->setCheckable(true);
+        action ->setChecked(child == activeSubWindow());
+        connect(action, SIGNAL(triggered()), m_windowMapper, SLOT(map()));
+        m_windowMapper->setMapping(action, child);
+    }
+}
+
+void MainWindow::updateViewsMenu()
+{
+    Q_ASSERT(m_viewsMenu);
+
+    // Clear menu
+    m_viewsMenu->clear();
+
+    // Get the list of all tool bars
+    QList<QDockWidget *> dockWidgets = qFindChildren<QDockWidget *>(this);
+
+    if (dockWidgets.size() != 0) {
+        // If the list is not empty
+        // Iterate through all dock widgets
+        for (int i = 0; i < dockWidgets.size(); ++i) {
+            if (dockWidgets.at(i)->parentWidget() == this)
+                // If the parent of dock widget is main window add toggled action to menu
+                m_viewsMenu->addAction(dockWidgets.at(i)->toggleViewAction());
+        }
+    }
+}
+
+QMdiSubWindow *MainWindow::activeSubWindow() const
+{
+    if (QMdiSubWindow *activeSubWindow = m_mdiArea.activeSubWindow())
+        return activeSubWindow;
+
+    return 0;
+}
+
+void MainWindow::onSubWindowActivated(QMdiSubWindow *subWindow)
+{}
+
+void MainWindow::setActiveSubWindow(QWidget *subWindow)
+{
+    if (!subWindow)
+        return;
+
+    m_mdiArea.setActiveSubWindow(qobject_cast<QMdiSubWindow *>(subWindow));
+}
+
+} // namespace gui
---
+++ gui/mainWindow.h
@@ -0,0 +1,176 @@
+/**************************************************************************
+*   Copyright (C) 2011 by Vahan Aghajanyan                                *
+*   vahancho@gmail.com                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef __MAINWINDOW_H__
+#define __MAINWINDOW_H__
+
+#include <QMainWindow>
+#include <QMdiArea>
+#include <QSystemTrayIcon>
+
+class QSignalMapper;
+class QProgressBar;
+
+namespace gui
+{
+
+/// Implements the Application's Main Window class.
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    /// The constructor.
+    MainWindow(QWidget *parent = 0, Qt::WFlags flags = 0);
+
+    /// Destructor.
+    ~MainWindow();
+
+    /// Restores the window's state based on defaults values.
+    /*!
+        Window's state is saved on closing the Application and restore on
+        next session. State includes window's size, position, dock windows
+        geometries and tool bars state.
+    */
+    void restoreWindow();
+
+protected:
+    /// Handles the window's close event.
+    /*!
+        Saves MainWindow geometry and settings.
+    */
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+
+    /// Opens Options dialog.
+    void onToolsOptions();
+
+    /// This slot is called when Tool bars sub menu is about to show.
+    void updateToolBarsMenu();
+
+    /// This slot is called when Views sub menu is about to show.
+    void updateViewsMenu();
+
+    /// This slot is called when Window menu is about to show.
+    void updateWindowMenu();
+
+    /// This slot is called when Status bar action is triggered.
+    void onStatusBarShowHide();
+
+    /// This slot called when a MDI sub window activated.
+    /*!
+        @param subWindow The MDI child window that has been activated.
+        \sa setActiveSubWindow(), activeSubWindow()
+    */
+    void onSubWindowActivated(QMdiSubWindow *subWindow);
+
+    /// This slot activates MDI sub window.
+    /*!
+        @param subWindow The MDI child window that will be activated.
+        \sa activeSubWindow(), onSubWindowActivated()
+    */
+    void setActiveSubWindow(QWidget *subWindow);
+
+    void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
+
+    void trayIconMessageClicked();
+
+    /// This slot called when About menu action triggered.
+    void onAbout();
+
+private:
+
+    /// Saves the window state.
+    void saveWindow() const;
+
+    /// Returns the active MDI sub window.
+    /*!
+        Returns the active MDI sub window if any, otherwise returns 0.
+        \sa setActiveSubWindow(), onSubWindowActivated()
+    */
+    QMdiSubWindow *activeSubWindow() const;
+
+    /// Construct and add menus.
+    void createMenus();
+
+    /// Create dockable windows.
+    void createDockWindows();
+
+    /// Registers and set Main Window defaults.
+    void addDefaults() const;
+
+    /// Creates the system tray icon.
+    void createTrayIcon();
+
+    void storeInClipboard(const QString &text) const;
+
+    /// The system tray icon.
+    QSystemTrayIcon *m_trayIcon;
+
+    /// Store the "Status bar" action.
+    QAction *m_statusBarAction;
+
+    /// Store the "Close" action.
+    QAction *m_closeAct;
+
+    /// Store the "Close all" action.
+    QAction *m_closeAllAct;
+
+    /// Store the "Tile" action.
+    QAction *m_tileAct;
+
+    /// Store the "Cascade" action.
+    QAction *m_cascadeAct;
+
+    /// Store the "Next" action.
+    QAction *m_nextAct;
+
+    /// Store the "Previous" action.
+    QAction *m_previousAct;
+
+    /// Store the separator action.
+    QAction *m_separatorAct;
+
+    /// Store the Window menu.
+    QMenu *m_windowMenu;
+
+    /// Store the 'Toolbars' menu.
+    QMenu *m_toolBarsMenu;
+
+    /// Store the 'Views' menu.
+    QMenu *m_viewsMenu;
+
+    /// The signal mapper for MDI child windows.
+    QSignalMapper *m_windowMapper;
+
+    /// Workspace for MDI application.
+    QMdiArea m_mdiArea;
+
+    /// The progress bar.
+    /*!
+        The progress bar for any task progress visualization.
+    */
+    QProgressBar *m_progressBar;
+};
+
+} // namespace gui
+
+#endif // __MAINWINDOW_H__
