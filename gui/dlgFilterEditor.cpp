d6a78cfe5de4e2c606d68e17e9b78879eb774a38
Vahan Aghajanyan <vahancho@gmail.com>
2011-11-16 23:09:39 +0100

    Extend the dialog and add basic controls.

--- gui/dlgFilterEditor.cpp
+++ gui/dlgFilterEditor.cpp
@@ -18,7 +18,15 @@
 *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 ***************************************************************************/
 
+#include <QLineEdit>
+#include <QGroupBox>
+#include <QLayout>
+#include <QRadioButton>
+#include <QLabel>
+#include <QTreeWidget>
+#include <QPushButton>
 #include "dlgFilterEditor.h"
+#include "../strings/guiStrings.h"
 
 namespace gui
 {
@@ -27,8 +35,48 @@ FilterEditor::FilterEditor(QWidget *parent, Qt::WindowFlags f)
     :
         QDialog(parent, f)
 {
-    
-}
+    QLabel *lblName = new QLabel("Name:", this);
+    QLineEdit *editName = new QLineEdit(this);
+    QHBoxLayout *nameLayout = new QHBoxLayout;
+    nameLayout->addWidget(lblName);
+    nameLayout->addWidget(editName);
 
+    QGroupBox *groupBox = new QGroupBox("Matching Criterion", this);
+    QRadioButton *radio1 = new QRadioButton("&All rules matched");
+    QRadioButton *radio2 = new QRadioButton("One of the rules matched");
+    radio1->setChecked(true);
+
+    QHBoxLayout *radioLayout = new QHBoxLayout;
+    radioLayout->addWidget(radio1);
+    radioLayout->addWidget(radio2);
+    radioLayout->addStretch(1);
+    groupBox->setLayout(radioLayout);
+
+    QTreeWidget *rules = new QTreeWidget;
+    rules->setColumnCount(3);
+    rules->setRootIsDecorated(false);
+    QStringList headerLabels;
+    headerLabels << "Property" << "Option" << "Value";
+    rules->setHeaderLabels(headerLabels);
+
+    QPushButton *btnOk = new QPushButton(str::Ok, this);
+    connect(btnOk, SIGNAL(clicked()), this, SLOT(accept()));
+    btnOk->setDefault(true);
+    QPushButton *btnCancel = new QPushButton(str::Cancel, this);
+    connect(btnCancel, SIGNAL(clicked()), this, SLOT(reject()));
+
+    QHBoxLayout *btnLayout = new QHBoxLayout;
+    btnLayout->addStretch();
+    btnLayout->addWidget(btnOk);
+    btnLayout->addWidget(btnCancel);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(nameLayout);
+    mainLayout->addWidget(groupBox);
+    mainLayout->addWidget(rules);
+    mainLayout->addLayout(btnLayout);
+
+    setLayout(mainLayout);
+}
 
 } // namespace gui
