2f0bca1f73b5a67d57466d9a80d831486ca14aa5
Vahan Aghajanyan <vahancho@gmail.com>
2012-01-04 01:15:30 +0100

    Implemented wrapper for the response on login to LJ server request.

---
+++ ljcommunicator/ljuserinfo.h
@@ -0,0 +1,51 @@
+/**************************************************************************
+*   Copyright (C) 2012 by Vahan Aghajanyan                                *
+*   vahancho@gmail.com                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#ifndef __LJUSERINFO_H__
+#define __LJUSERINFO_H__
+
+#include "export.h"
+#include "response.h"
+
+namespace lj
+{
+
+/// Implements the wrapper class around response for the login request.
+class LJCOMMUNICATOR_EXPORT UserInfo : public xmlrpc::Response
+{
+public:
+    UserInfo();
+    UserInfo(const QByteArray &data);
+
+    /// Returns user's full name.
+    QString fullName() const;
+
+    /// Returns LJ message to the user.
+    QString message() const;
+
+private:
+    QMap<QString, QVariant> m_info;
+};
+
+} // namespace lj
+
+#endif // __LJUSERINFO_H__
+
+
