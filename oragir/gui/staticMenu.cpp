9153c2aef36c9608fb386ad5551ef132e1086e64
Vahan Aghajanyan <vahancho@gmail.com>
2012-02-24 22:25:55 +0100

    Implemented new custom menu that supports multiselection.
    This new menu does not close as soon as any action has triggered, but
    allows to trigger another actions while it stays opened.

---
+++ oragir/gui/staticMenu.cpp
@@ -0,0 +1,84 @@
+/**************************************************************************
+*   Copyright (C) 2012 by Vahan Aghajanyan                                *
+*   vahancho@gmail.com                                                    *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+***************************************************************************/
+
+#include <QEvent>
+#include <QDebug>
+#include "staticMenu.h"
+
+namespace gui
+{
+
+StaticMenu::StaticMenu(QWidget *parent)
+    :
+        QMenu(parent),
+        m_currentAction(0)
+{
+    init();
+}
+
+StaticMenu::StaticMenu(const QString &title, QWidget *parent)
+    :
+        QMenu(title, parent),
+        m_currentAction(0)
+{
+    init();
+}
+
+StaticMenu::~StaticMenu()
+{}
+
+bool StaticMenu::eventFilter(QObject *obj, QEvent *event)
+{
+    if (event->type() == QEvent::MouseButtonRelease) {
+        if (m_currentAction && m_currentAction->isCheckable()) {
+            m_currentAction->setChecked(!m_currentAction->isChecked());
+            emit triggered(m_currentAction);
+        }
+        return true;
+    } else if (event->type() == QEvent::Hide ||
+               event->type() == QEvent::HoverLeave) {
+        m_currentAction = 0;
+    }
+    // Propagate to the parent class.
+    return QObject::eventFilter(obj, event);
+}
+
+void StaticMenu::onActionHovered(QAction *act)
+{
+    m_currentAction = act;
+}
+
+void StaticMenu::init()
+{
+    installEventFilter(this);
+    connect(this, SIGNAL(hovered(QAction *)),
+            this, SLOT(onActionHovered(QAction *)));
+}
+
+void StaticMenu::uncheckAll()
+{
+    QList<QAction *> actionList = actions();
+    foreach (QAction *action, actionList) {
+        if (action->isCheckable())
+            action->setChecked(false);
+    }
+}
+
+} // namespace gui
